c7da24f748192a8b1e664ac0d45ee66418336f64
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-02-13 02:16:18 +0300

    Realize methods

--- src/model/Model.py
+++ src/model/Model.py
@@ -1,15 +1,18 @@
+from src.model.AlgorithmInterface import AlgorithmInterface
+
 class AlgorithmModel:
-    def __init__(self, algorithm_strategy=None):
-        self.algorithm_strategy = algorithm_strategy
+    def __init__(self, strategy=None):
+        self.strategy = strategy
 
     def set_algorithm(self, algorithm_strategy):
-        self.algorithm_strategy = algorithm_strategy
+        self.strategy = algorithm_strategy
 
-    def get_graph(self):
-        pass
+    def graph(self):
+        return self.strategy.graph()
 
-    def execute_algorithm(self, params):
-        pass
+    def execute(self):
+        solutions = self.strategy.execute()
+        return solutions
 
     def add_observer(self, observer):
-        self.algorithm_strategy.add_observer(observer)
+        self.strategy.add_observer(observer)
