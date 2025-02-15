0eaa2fc8bc45ea4d769ba3894b8bdeeaf33d702b
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-02-15 21:19:40 +0300

    View make passive and resends all actions to Presenter

---
+++ src/views/MainView.py
@@ -0,0 +1,20 @@
+from PyQt6.QtWidgets import QMainWindow
+from src.views.ViewInterface import ViewInterface
+from src.views.mainview import Ui_MainWindow
+
+class MainView(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.presenter = None
+
+    def set_presenter(self, presenter):
+        self.presenter = presenter
+
+    def set_plot(self):
+        self.presenter.set_plot()
+
+    def execute(self):
+        self.presenter.execute()
