0c11a968f549015ab462967d96f782728fafbc3a
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-02-21 22:39:07 +0300

    Refactoring: Remove method surface_params from AlgorithmInterface

--- src/model/AlgorithmInterface.py
+++ src/model/AlgorithmInterface.py
@@ -3,13 +3,9 @@ from abc import ABC, abstractmethod
 class AlgorithmInterface(ABC):
 
     @abstractmethod
-    def set_params(self, **params):
+    def set_params(self, function, **params):
         pass
 
     @abstractmethod
     def execute(self):
-        pass
-
-    @abstractmethod
-    def surface_params(self) -> dict:
         pass
