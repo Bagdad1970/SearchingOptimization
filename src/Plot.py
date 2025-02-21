aeb16d14e551597ba4547b29dc418e301c12fee0
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-02-21 22:23:41 +0300

    Rename: PlotWidget rename to Plot

--- src/Plot.py
+++ src/Plot.py
@@ -1,8 +1,9 @@
 import numpy
 import numpy as np
 import pyqtgraph.opengl as gl
-from pyqtgraph.opengl import GLViewWidget, GLSurfacePlotItem
+from pyqtgraph.opengl import GLViewWidget, GLSurfacePlotItem, GLScatterPlotItem
 
+from src.entities.Point import Point
 
 class PlotWidget(GLViewWidget):
     def __init__(self):
@@ -11,17 +12,24 @@ class PlotWidget(GLViewWidget):
         self.set_camera()
         self.add_grid()
 
+        self.scatter_plot = GLScatterPlotItem()
+        self.addItem(self.scatter_plot)
+
     def set_background(self):
         self.setBackgroundColor('k')
 
     def set_camera(self):
         self.setCameraPosition(distance=20, elevation=25, azimuth=45)
 
-    def add_grid(self, size=20, spacing=2):
+    def add_grid(self, size=20, spacing=1):
         grid = gl.GLGridItem()
         grid.setSize(size, size)
         grid.setSpacing(spacing, spacing)
         self.addItem(grid)
+
+    def set_point(self, point: Point):
+        pos = np.array([point.get_vector()], dtype=np.float32)
+        self.scatter_plot.setData(pos=pos, size=8, color=(1, 0, 0, 1), pxMode=True)
 
     def set_surface(self, params):
         x, y, function = params['x'], params['y'], params['function']
@@ -29,6 +37,8 @@ class PlotWidget(GLViewWidget):
         Z = function(X, Y)
 
         surface = gl.GLSurfacePlotItem(x=x, y=y, z=Z, shader='heightColor', smooth=True)
+
+        self.set_point(Point([1, 2, 3]))
         #old_surface = (item for item in self.items if isinstance(item, GLSurfacePlotItem))
         #print(type(old_surface))
         #if old_surface is not None:
