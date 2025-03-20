8918fa7c66ed74631a6e1a2635cd39f868fa6394
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-20 23:23:39 +0300

    Add method full_point()
    full_point() returns point with added function value by this point

--- src/entities/point.py
+++ src/entities/point.py
@@ -3,24 +3,46 @@ import numpy as np
 
 
 class Point:
+    """
+    Represents coordinate point in n-dimensional space
+    """
+
     def __init__(self, point=None):
         self.point = point if point is not None else []
 
     def create_points_array(self, *, x_length: float, y_length: float) -> tuple:
-        x_left_length, x_right_length = math.ceil( (self.point[0] - x_length) / 2), math.ceil( (self.point[0] + x_length) / 2)
-        y_left_length, y_right_length = math.ceil( (self.point[1] - y_length) / 2), math.ceil( (self.point[1] + y_length) / 2)
-        return (np.linspace(x_left_length, x_right_length, 100),
-                np.linspace(y_left_length, y_right_length, 100))
+        x_left_length, x_right_length = math.ceil(
+            (self.point[0] - x_length) / 2
+        ), math.ceil((self.point[0] + x_length) / 2)
+        y_left_length, y_right_length = math.ceil(
+            (self.point[1] - y_length) / 2
+        ), math.ceil((self.point[1] + y_length) / 2)
+        return (
+            np.linspace(x_left_length, x_right_length, 100),
+            np.linspace(y_left_length, y_right_length, 100),
+        )
+
+    @classmethod
+    def full_point(cls, point, function):
+        return Point(point.get_point() + [function(*point)])
 
     def get_point(self):
         return self.point
 
     def equalid_norm(self):
-        return sum(x ** 2 for x in self.point) ** 0.5
+        return sum(x**2 for x in self.point) ** 0.5
 
     def scalar_multiply(self, scalar):
         """Умножение вектора на скаляр"""
         return Point([scalar * x for x in self.point])
+
+    def __add__(self, other):
+        return Point(
+            [
+                self.point[i] + other[i]
+                for i in range(min(len(self.point), len(other.point)))
+            ]
+        )
 
     def __iter__(self):
         """Позволяет итерировать по элементам вектора"""
@@ -28,8 +50,8 @@ class Point:
 
     def __sub__(self, other):
         """
-        Вычитание двух векторов
-        :param other: Вектор, который вычитается из текущего
+        Substract first point from second
+        :param other: Substracted point
         """
         return Point([a - b for a, b in zip(self.point, other)])
 
@@ -49,12 +71,10 @@ class Point:
         return len(self.point)
 
     def __getitem__(self, index):
-        """Позволяет доступ к элементам по индексу: v[0], v[1]"""
         return self.point[index]
 
     def __setitem__(self, index, value):
-        """Позволяет изменять элементы по индексу: v[0] = 10"""
         self.point[index] = value
 
     def __repr__(self):
-        return f"Point({self.point})"
+        return f"Point({self.point!r})"
