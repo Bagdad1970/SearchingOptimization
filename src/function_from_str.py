270f169f36eb7bf8b476729d9463551ef95fcfba
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-03-24 00:57:58 +0300

    Add sorting of symbols in function

--- src/function_from_str.py
+++ src/function_from_str.py
@@ -1,6 +1,10 @@
 import sympy
 
 def function_from_str(function: str):
-    x, y = sympy.symbols('x1 x2')
     expression = sympy.sympify(function)
-    return sympy.lambdify((x, y), expression)
+    func_symbols = sort_symbols_by_ord(expression.free_symbols)
+    return sympy.lambdify(func_symbols, expression)
+
+def sort_symbols_by_ord(symbols: set):
+    str_symbols = sorted(map(str, symbols), key=lambda s: sum(ord(c) for c in s))
+    return sympy.symbols(' '.join(str_symbols))
