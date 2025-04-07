560cb500fdd85d8f5ca428c1486ecc92a3fc3259
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-04-07 23:38:19 +0300

    Point inherits from UserList

--- src/entities/point.py
+++ src/entities/point.py
@@ -1,22 +1,23 @@
 import math
 import numpy as np
+from collections import UserList
 
 
-class Point:
+class Point(UserList):
     """
     Represents coordinate point in n-dimensional space
     """
 
     def __init__(self, point=None):
-        self.point = point if point is not None else []
+        super().__init__(point if point is not None else [])
 
     def create_points_array(self, *, x_length: float, y_length: float) -> tuple:
         x_left_length, x_right_length = math.ceil(
-            (self.point[0] - x_length) / 2
-        ), math.ceil((self.point[0] + x_length) / 2)
+            (self.data[0] - x_length) / 2
+        ), math.ceil((self.data[0] + x_length) / 2)
         y_left_length, y_right_length = math.ceil(
-            (self.point[1] - y_length) / 2
-        ), math.ceil((self.point[1] + y_length) / 2)
+            (self.data[1] - y_length) / 2
+        ), math.ceil((self.data[1] + y_length) / 2)
         return (
             np.linspace(x_left_length, x_right_length, 100),
             np.linspace(y_left_length, y_right_length, 100),
@@ -28,54 +29,38 @@ class Point:
         return Point([*point, function_value])
 
     def get_point(self):
-        return self.point
+        return self.data
 
     def equalid_norm(self):
-        return sum(x**2 for x in self.point) ** 0.5
+        return sum(x**2 for x in self.data) ** 0.5
 
     def scalar_multiply(self, scalar):
         """Умножение вектора на скаляр"""
-        return Point([scalar * x for x in self.point])
+        return Point([scalar * x for x in self.data])
 
     def __add__(self, other):
         return Point(
             [
-                self.point[i] + other[i]
-                for i in range(min(len(self.point), len(other.point)))
+                self.data[i] + other[i]
+                for i in range(min(len(self.data), len(other)))
             ]
         )
-
-    def __iter__(self):
-        """Позволяет итерировать по элементам вектора"""
-        return iter(self.point)
 
     def __sub__(self, other):
         """
         Substract first point from second
         :param other: Substracted point
         """
-        return Point([a - b for a, b in zip(self.point, other)])
-
-    def append(self, value):
-        self.point.append(value)
+        return Point([a - b for a, b in zip(self.data, other)])
 
     def copy(self):
-        return Point(self.point.copy())
+        return Point(self.data.copy())
 
     def __eq__(self, other):
         """Проверка на равенство векторов"""
         if isinstance(other, Point):
-            return self.point == other.point
+            return self.data == other.data
         return False
 
-    def __len__(self):
-        return len(self.point)
-
-    def __getitem__(self, index):
-        return self.point[index]
-
-    def __setitem__(self, index, value):
-        self.point[index] = value
-
     def __repr__(self):
-        return f"Point({self.point!r})"
+        return f"Point({self.data!r})"
