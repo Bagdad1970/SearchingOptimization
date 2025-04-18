75c188ce4764c554c235f68629f26db86be32855
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-04-18 23:32:19 +0300

    Create package with options classes

---
+++ src/views/options_views/options/__init__.py
@@ -0,0 +1,11 @@
+from .genetic_algorithm import GeneticAlgorithmOptions
+from .gradient_descent import GradientDescentOptions
+from .particle_swarm import ParticleSwarmOptions
+from .simplex_method import SimplexMethodOptions
+
+__all__ = [
+    "GeneticAlgorithmOptions",
+    "GradientDescentOptions",
+    "ParticleSwarmOptions",
+    "SimplexMethodOptions",
+]
