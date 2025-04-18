96a2f9dd14fc5d633b6a15d32e970b2dd61d9f41
Bogdan Belokobylskiy <BogdanBattle@mail.ru>
2025-04-18 23:31:43 +0300

    Create package with algorithms

---
+++ src/model/strategies/__init__.py
@@ -0,0 +1,11 @@
+from .genetic_algorithm import GeneticAlgorithm
+from .simplex_method import SimplexMethod
+from .gradient_descent import GradientDescent
+from .particle_swarm import ParticleSwarm
+
+__all__ = [
+    "GeneticAlgorithm",
+    "GradientDescent",
+    "ParticleSwarm",
+    "SimplexMethod",
+]
